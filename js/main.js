72b4162bc1b7c2845ec2678d7a64d6b418d7b12e
stella99me <1781976921@qq.com>
2016-03-02 17:48:05 +0800

    nav
    nav

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 $(function(){
 	$(".page-scroll").bind("click",function(event){
 		var $anchor = $(this);
-		$anchor.addClass("active");
-		$anchor.siblings("a").removeClass("active")
+		$anchor.parent().addClass("active");
+		$anchor.parent().siblings("li").removeClass("active")
 		$("html,body").stop().animate({
 			scrollTop:$($anchor.attr("href")).offset().top
 		},1500,'easeInOutExpo')
